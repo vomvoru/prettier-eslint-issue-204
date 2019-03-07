3ace14d7db027c2a92cbc2b3905f9313f70a1d05
Park Cheol Hyeon <pch33@tmon.co.kr>
2019-03-07 19:35:31 +0900

    remove comments

--- .eslintrc.airbnb.babel-eslint.js
+++ .eslintrc.airbnb.babel-eslint.js
@@ -11,9 +11,7 @@ module.exports = {
     jest: true,
   },
   rules: {
-    // mono repo 방식을 써서 제대로 동작하지 않는 eslint 규칙 임시로 비활성화.
     'import/no-extraneous-dependencies': 'off',
-    // eslint 파일이 있는곳을 기준으로 package 여부를 확인하기 때문에 임시로 비활성화.
     'import/no-unresolved': 'off',
   },
 };
